a5f3413d6169f9daae16099ebf18b9c497b7c37d
Kalyan27112 <Thodupunuri.27112@stu.upes.ac.in>
2025-10-30 21:47:55 +0530

    Day22A_strong number.c

---
+++ Day22A_strong number.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+// Function to calculate factorial of a digit
+int factorial(int n) {
+    int fact = 1;
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
+int main() {
+    int num, temp, digit, sum = 0;
+
+    // Input a number
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    temp = num; // Store original number
+
+    // Find sum of factorials of digits
+    while (temp > 0) {
+        digit = temp % 10;
+        sum += factorial(digit);
+        temp /= 10;
+    }
+
+    // Check if number is strong
+    if (sum == num)
+        printf("%d is a Strong Number.\n", num);
+    else
+        printf("%d is not a Strong Number.\n", num);
+
+    return 0;
+}
