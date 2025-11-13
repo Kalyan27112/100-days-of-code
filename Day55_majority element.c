7d0c387d36353dc3392af682caef58fa01681a5b
Kalyan27112 <Thodupunuri.27112@stu.upes.ac.in>
2025-11-13 13:41:27 +0530

    Day55_majority element.c

---
+++ Day55_majority element.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int nums[n];
+    printf("Enter array elements:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+
+    // Boyer–Moore Voting Algorithm to find candidate
+    int candidate = 0, count = 0;
+    for (int i = 0; i < n; i++) {
+        if (count == 0) {
+            candidate = nums[i];
+            count = 1;
+        } else if (nums[i] == candidate) {
+            count++;
+        } else {
+            count--;
+        }
+    }
+
+    // Verify candidate
+    count = 0;
+    for (int i = 0; i < n; i++) {
+        if (nums[i] == candidate) {
+            count++;
+        }
+    }
+
+    if (count > n / 2)
+        printf("%d\n", candidate);
+    else
+        printf("-1\n");
+
+    return 0;
+}
