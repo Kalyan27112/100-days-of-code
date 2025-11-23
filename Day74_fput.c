0bc532dbaeb403186af3d4e8a8bead1ea551b5a5
Kalyan27112 <Thodupunuri.27112@stu.upes.ac.in>
2025-11-23 17:34:20 +0530

    Day74_fput.c

---
+++ Day74_fput.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    char srcFile[100], destFile[100];
+    FILE *src, *dest;
+    int ch;
+
+    // Take file names from the user
+    printf("Enter source file name: ");
+    scanf("%s", srcFile);
+
+    printf("Enter destination file name: ");
+    scanf("%s", destFile);
+
+    // Open source file
+    src = fopen(srcFile, "r");
+    if (src == NULL) {
+        printf("Error: Could not open source file %s\n", srcFile);
+        return 1;
+    }
+
+    // Open destination file
+    dest = fopen(destFile, "w");
+    if (dest == NULL) {
+        fclose(src);
+        printf("Error: Could not open destination file %s\n", destFile);
+        return 1;
+    }
+
+    // Copy content using fgetc() and fputc()
+    while ((ch = fgetc(src)) != EOF) {
+        fputc(ch, dest);
+    }
+
+    // Close both files
+    fclose(src);
+    fclose(dest);
+
+    printf("File copied successfully from %s to %s\n", srcFile, destFile);
+
+    return 0;
+}
