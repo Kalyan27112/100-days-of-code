0a1595c74b815941e52d22ebd4f76b894b4821d9
Kalyan27112 <Thodupunuri.27112@stu.upes.ac.in>
2025-11-23 17:16:09 +0530

    Day65_anagram.c

---
+++ Day65_anagram.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[1000005], t[1000005];
+
+    // Read input strings
+    scanf("%s", s);
+    scanf("%s", t);
+
+    // If lengths differ, they can't be anagrams
+    if (strlen(s) != strlen(t)) {
+        printf("Not Anagram");
+        return 0;
+    }
+
+    int freq[26] = {0};
+
+    // Count characters in s
+    for (int i = 0; s[i] != '\0'; i++) {
+        freq[s[i] - 'a']++;
+    }
+
+    // Subtract characters from t
+    for (int i = 0; t[i] != '\0'; i++) {
+        freq[t[i] - 'a']--;
+    }
+
+    // Check if all frequencies became zero
+    for (int i = 0; i < 26; i++) {
+        if (freq[i] != 0) {
+            printf("Not Anagram");
+            return 0;
+        }
+    }
+
+    printf("Anagram");
+    return 0;
+}
