30f873d6ebb7763ffa2d0207163b12b33b1f62a4
Kalyan27112 <Thodupunuri.27112@stu.upes.ac.in>
2025-11-23 18:23:48 +0530

    Day89_enum of gender.c

---
+++ Day89_enum of gender.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+enum Gender {
+    MALE = 1,
+    FEMALE,
+    OTHER
+};
+
+struct Person {
+    char name[50];
+    int age;
+    enum Gender gender;
+};
+
+int main() {
+    struct Person p;
+
+    // Input person details
+    printf("Enter name: ");
+    scanf("%s", p.name);
+
+    printf("Enter age: ");
+    scanf("%d", &p.age);
+
+    printf("Select Gender (1=MALE, 2=FEMALE, 3=OTHER): ");
+    scanf("%d", (int*)&p.gender);
+
+    // Output
+    printf("\n--- Person Details ---\n");
+    printf("Name: %s\n", p.name);
+    printf("Age: %d\n", p.age);
+
+    printf("Gender: ");
+    switch (p.gender) {
+        case MALE:   printf("Male\n"); break;
+        case FEMALE: printf("Female\n"); break;
+        case OTHER:  printf("Other\n"); break;
+        default:     printf("Invalid\n");
+    }
+
+    return 0;
+}
