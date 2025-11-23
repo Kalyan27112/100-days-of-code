250a46cdca206ec4fdbce82070bfe59030b9bb6c
Kalyan27112 <Thodupunuri.27112@stu.upes.ac.in>
2025-11-23 17:12:40 +0530

    Day63_kth smallest element.c

---
+++ Day63_kth smallest element.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Comparison function for qsort
+int compare(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int main() {
+    int n, k;
+
+    // Read array size
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Read array elements
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Read k
+    scanf("%d", &k);
+
+    // Sort array
+    qsort(arr, n, sizeof(int), compare);
+
+    // Print k-th smallest element (1-based)
+    printf("%d", arr[k - 1]);
+
+    return 0;
+}
