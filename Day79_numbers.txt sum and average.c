f87e5e6f5fa52e8781ead14d066095b33f48cd7d
Kalyan27112 <Thodupunuri.27112@stu.upes.ac.in>
2025-11-23 18:10:40 +0530

    Day79_numbers.txt sum and average.c

---
+++ Day79_numbers.txt sum and average.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    FILE *fp;
+    char filename[] = "numbers.txt";   // file name we keep
+    int num, count = 0;
+    long long sum = 0;
+
+    fp = fopen(filename, "r");
+    if (fp == NULL) {
+        printf("Error: Could not open file %s\n", filename);
+        return 1;
+    }
+
+    // Read integers from the file
+    while (fscanf(fp, "%d", &num) == 1) {
+        sum += num;
+        count++;
+    }
+
+    fclose(fp);
+
+    if (count == 0) {
+        printf("No numbers found in the file.\n");
+    } else {
+        double avg = (double)sum / count;
+
+        printf("Sum: %lld\n", sum);
+        printf("Average: %.2f\n", avg);
+    }
+
+    return 0;
+}
