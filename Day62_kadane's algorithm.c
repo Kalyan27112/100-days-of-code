be4e2827220a27c303e52ee51cd049ba6819120c
Kalyan27112 <Thodupunuri.27112@stu.upes.ac.in>
2025-11-23 17:08:47 +0530

    Day62_kadane's algorithm.c

---
+++ Day62_kadane's algorithm.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int maxSoFar = arr[0];
+    int currentSum = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        // Either extend previous subarray or start new from current element
+        if (currentSum + arr[i] > arr[i])
+            currentSum = currentSum + arr[i];
+        else
+            currentSum = arr[i];
+
+        // Update global max
+        if (currentSum > maxSoFar)
+            maxSoFar = currentSum;
+    }
+
+    printf("%d", maxSoFar);
+    return 0;
+}
