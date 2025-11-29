5d62bc56bde08453c218b8119eb1c3a2a2f5f626
Kalyan27112 <Thodupunuri.27112@stu.upes.ac.in>
2025-11-29 11:41:06 +0530

    Day02A_area.c

--- Day02A_area.c
+++ Day02A_area.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+Q3 (User Inputs, Operations & Output)
+//Write a program to calculate the area and perimeter of a rectangle given its length and breadth.#include <stdio.h>
 
 int main() {
     double length, breadth, area, perimeter;
