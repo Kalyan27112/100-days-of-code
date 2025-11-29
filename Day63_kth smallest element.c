b92559957c70f4c66e0162a8275b4f16156551b1
Kalyan27112 <Thodupunuri.27112@stu.upes.ac.in>
2025-11-29 15:13:28 +0530

    Day63_kth smallest element.c

--- Day63_kth smallest element.c
+++ Day63_kth smallest element.c
@@ -1,3 +1,7 @@
+Q113 (Logic Enhancers)
+//Write a program to take an integer array arr and an integer k as inputs. The task is to find the
+kth smallest element in the array. Print the kth smallest element as output.
+
 #include <stdio.h>
 #include <stdlib.h>
 
