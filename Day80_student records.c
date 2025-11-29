2f32bbd817221b604b9177b0b798f255edacfc1f
Kalyan27112 <Thodupunuri.27112@stu.upes.ac.in>
2025-11-29 15:36:19 +0530

    Day80_student records.c

--- Day80_student records.c
+++ Day80_student records.c
@@ -1,3 +1,6 @@
+Q130 (File Handling)
+//Store multiple student records (name, roll number, marks) into a file using fprintf(). Then read them using fscanf() and display each record.
+
 #include <stdio.h>
 
 int main() {
