efa2fda875d7dad066bfe7a75943f98da618b380
Kalyan27112 <Thodupunuri.27112@stu.upes.ac.in>
2025-11-29 13:37:33 +0530

    Day53_find pivot index.c

--- Day53_find pivot index.c
+++ Day53_find pivot index.c
@@ -1,5 +1,9 @@
-#include <stdio.h>
 
+Q103//Write a Program to take a sorted array arr[] and an integer x as input, find the index (0-based)
+of the smallest element in arr[] that is greater than or equal to x and print it. This element is
+called the ceil of x. If such an element does not exist,print -1. 
+Note: In case of multiple occurrences of ceil of x, return the index of the first occurrence.
+#include <stdio.h>
 int findPivotIndex(int arr[], int n) {
     int totalSum = 0;
     for (int i = 0; i < n; i++)
