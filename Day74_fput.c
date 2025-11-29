a53bcba32cada8f0b3d87ab3d71aaa4f81739cd2
Kalyan27112 <Thodupunuri.27112@stu.upes.ac.in>
2025-11-29 15:32:05 +0530

    Day74_fput.c

--- Day74_fput.c
+++ Day74_fput.c
@@ -1,3 +1,7 @@
+Q124 (File Handling)
+//Take two filenames from the user – a source file and a destination file. Copy all the content from the source file to the destination file
+using fgetc() and fputc().
+
 #include <stdio.h>
 
 int main() {
